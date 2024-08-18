03b1c5328d6ebf2c5e47f26d41ce21c113694802
Guilherme Monção <guilherme_oliveira27@outlook.com>
2024-08-18 14:33:27 -0400

    Feat: Finishing the services

--- src/main/java/com/start/spring/app/service/DepartamentoService.java
+++ src/main/java/com/start/spring/app/service/DepartamentoService.java
@@ -1,5 +1,17 @@
 package com.start.spring.app.service;
 
-public interface DepartamentoService {
+import java.util.List;
 
+import com.start.spring.app.domain.Departamento;
+
+public interface DepartamentoService {
+	void salvar(Departamento departamento);
+	
+	void editar(Departamento departamento);
+	
+	void excluir(Long id);
+	
+	Departamento buscarPorId(Long id);
+	
+	List<Departamento> buscarTodos();
 }
--- src/main/java/com/start/spring/app/service/FuncionarioService.java
+++ src/main/java/com/start/spring/app/service/FuncionarioService.java
@@ -1,5 +1,17 @@
 package com.start.spring.app.service;
 
-public interface FuncionarioService {
+import java.util.List;
 
+import com.start.spring.app.domain.Funcionario;
+
+public interface FuncionarioService {
+void salvar(Funcionario funcionario);
+	
+	void editar(Funcionario funcionario);
+	
+	void excluir(Long id);
+	
+	Funcionario buscarPorId(Long id);
+	
+	List<Funcionario> buscarTodos();
 }
