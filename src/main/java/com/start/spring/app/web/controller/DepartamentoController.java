d8d55ed50a58de1d94fc72b37a48fca12502b9f5
Guilherme Monção <guilherme_oliveira27@outlook.com>
2024-08-25 14:19:50 -0400

    Feat: Listing departments

--- src/main/java/com/start/spring/app/web/controller/DepartamentoController.java
+++ src/main/java/com/start/spring/app/web/controller/DepartamentoController.java
@@ -2,6 +2,7 @@ package com.start.spring.app.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,8 @@ public class DepartamentoController {
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", service.buscarTodos());
 		return "/departamento/lista";
 	}
 
